3c9e6bd820dfad6aededdf9c58cf6d79d868ccf0
Brian Flad <bflad417@gmail.com>
2013-05-11 14:12:37 -0400

    Adjusted docker cookbook minitests

--- files/default/tests/minitest/default_test.rb
+++ files/default/tests/minitest/default_test.rb
@@ -2,9 +2,5 @@ require File.expand_path('../support/helpers', __FILE__)
 
 describe_recipe "docker::default" do
   include Helpers::Docker
-
-  it 'starts docker' do
-    service("docker").must_be_running
-  end
   
 end
---
+++ files/default/tests/minitest/upstart_test.rb
@@ -0,0 +1,10 @@
+require File.expand_path('../support/helpers', __FILE__)
+
+describe_recipe "docker::upstart" do
+  include Helpers::Docker
+
+  it 'starts docker' do
+    service("docker").must_be_running
+  end
+  
+end
