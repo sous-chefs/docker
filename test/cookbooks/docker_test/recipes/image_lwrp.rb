881444bb4db63cc31e249aa04603bf5a66eea651
Christian Fischer <sw-dev@computerlyrik.de>
2013-09-25 22:37:57 +0200

    added some test scenario for building

--- test/cookbooks/docker_test/recipes/image_lwrp.rb
+++ test/cookbooks/docker_test/recipes/image_lwrp.rb
@@ -23,3 +23,19 @@ docker_image "busybox"
 docker_image "base" do
   action :remove
 end
+
+fname = "/tmp/docker_image_build.dockerfile"
+
+cookbook_file fname do
+  source "Dockerfile"
+end
+
+docker_image "myImage" do
+  tag "myTag"
+  dockerfile fname
+  action :build
+end
+
+docker_image "myImage" do
+  action :remove
+end
