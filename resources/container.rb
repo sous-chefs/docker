51ac7be4daf64637eda1af722ff6e9ba05287606
Brian Flad <bflad417@gmail.com>
2013-11-10 22:35:48 -0500

    Alphabetize attributes in container and image resources for readability

--- resources/container.rb
+++ resources/container.rb
@@ -3,6 +3,8 @@ actions :remove, :restart, :run, :start, :stop
 default_action :run
 
 attribute :image, :name_attribute => true
+
+attribute :cmd_timeout, :kind_of => [Integer], :default => 60
 attribute :command, :kind_of => [String]
 attribute :detach, :kind_of => [TrueClass, FalseClass]
 attribute :env, :kind_of => [String]
@@ -10,12 +12,11 @@ attribute :hostname, :kind_of => [String]
 attribute :id, :kind_of => [String]
 attribute :memory, :kind_of => [Fixnum]
 attribute :port, :kind_of => [Fixnum]
+attribute :privileged, :kind_of => [TrueClass, FalseClass]
 attribute :public_port, :kind_of => [Fixnum]
 attribute :running, :kind_of => [TrueClass, FalseClass]
-attribute :tty, :kind_of => [TrueClass, FalseClass]
 attribute :stdin, :kind_of => [TrueClass, FalseClass]
-attribute :privileged, :kind_of => [TrueClass, FalseClass]
+attribute :tty, :kind_of => [TrueClass, FalseClass]
 attribute :user, :kind_of => [String]
 attribute :volume, :kind_of => [String]
 attribute :working_directory, :kind_of => [String]
-attribute :cmd_timeout, :kind_of => [Integer], :default => 60
--- resources/image.rb
+++ resources/image.rb
@@ -3,13 +3,14 @@ actions :pull, :remove, :build, :import
 default_action :pull
 
 attribute :image_name, :name_attribute => true
+
+attribute :cmd_timeout, :kind_of => [Integer], :default => 300
+attribute :dockerfile, :kind_of => [String]
 attribute :id, :kind_of => [String]
+attribute :image_url, :kind_of => [String]
 attribute :installed, :kind_of => [TrueClass, FalseClass]
 attribute :installed_tag, :kind_of => [String]
+attribute :path, :kind_of => [String]
 attribute :registry, :kind_of => [String]
 attribute :repository, :kind_of => [String]
 attribute :tag, :kind_of => [String]
-attribute :dockerfile, :kind_of => [String]
-attribute :image_url, :kind_of => [String]
-attribute :cmd_timeout, :kind_of => [Integer], :default => 300
-attribute :path, :kind_of => [String]
