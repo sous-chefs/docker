2721fe15d594e2211ad4913bfdc38238fa244d42
Jay Faulkner <jay@jvf.cc>
2013-12-19 14:38:14 -0800

    fix(aufs): Check that the package exists before installing
    - Also for new method, use ohai's provided kernel release rather than
      shelling out to uname

--- recipes/aufs.rb
+++ recipes/aufs.rb
@@ -3,13 +3,13 @@ when 'ubuntu'
   # If aufs isn't available, do our best to install the correct linux-image-extra package.
   if node['docker']['aufs']['legacy_package_finder']
     # Original method copied from https://github.com/thoward/docker-cookbook/blob/master/recipes/default.rb
-    extra_package = Mixlib::ShellOut.new("apt-cache search linux-image-extra-`uname -r | grep --only-matching -e [0-9]\.[0-9]\.[0-9]-[0-9]*` | cut -d ' ' -f 1").run_command.stdout.strip
+    package_name = 'linux-image-extra-' + Mixlib::ShellOut.new("uname -r | grep --only-matching -e [0-9]\.[0-9]\.[0-9]-[0-9]*").run_command.stdout.strip
   else
-    # In modern ubuntu versions, uname -r matches the kernel package name
-    uname = Mixlib::ShellOut.new('uname -r').run_command.stdout.strip
-    extra_package = 'linux-image-extra-' + uname
+    # In modern ubuntu versions, kernel release matches the kernel package name
+    package_name = 'linux-image-extra-' + node['kernel']['release']
   end
 
+  extra_package = Mixlib::ShellOut.new('apt-cache search ' + package_name).run_command.stdout.split(' ').first.strip
   unless extra_package.empty?
     package extra_package do
       not_if 'modprobe -l | grep aufs'
