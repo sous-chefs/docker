f1c59328998c7f00ea26c385deeb2740340ba2d3
Jay Faulkner <jay@jvf.cc>
2013-12-18 18:22:36 -0800

    fix(extras): Simplify how we find extras package
    - Current regexp breaks on kernels with double-digit versions (i.e.
      3.11.0-13-generic)
    - Can't find a sensible reason why we can't plug in uname -r directly

--- recipes/aufs.rb
+++ recipes/aufs.rb
@@ -1,14 +1,8 @@
 case node['platform']
 when 'ubuntu'
-  #
-  # The below code copied from: https://github.com/thoward/docker-cookbook/blob/master/recipes/default.rb
-  # It's not pretty, but gets the job done!
-  #
-  # If aufs isn't available, do our best to install the correct
-  # linux-image-extra package. This is somewhat messy because the
-  # naming of these packages is very inconsistent across kernel
-  # versions
-  extra_package = Mixlib::ShellOut.new("apt-cache search linux-image-extra-`uname -r | grep --only-matching -e [0-9]\.[0-9]\.[0-9]-[0-9]*` | cut -d ' ' -f 1").run_command.stdout.strip
+  # If aufs isn't available, do our best to install the correct linux-image-extra package.
+  uname = Mixlib::ShellOut.new('uname -r').run_command.stdout.strip
+  extra_package = 'linux-image-extra-' + uname
   unless extra_package.empty?
     package extra_package do
       not_if 'modprobe -l | grep aufs'
