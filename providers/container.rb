de097a43b1c62bb5549ef9e29456a349edad09cc
Jay Faulkner <jay@jvf.cc>
2014-01-03 11:18:46 -0800

    fix(whitespace): squeeze before printing log

--- providers/container.rb
+++ providers/container.rb
@@ -10,7 +10,7 @@ def load_current_resource
   dps.stdout.each_line do |dps_line|
     next unless dps_line.include?(new_resource.image)
     next if new_resource.command && !dps_line.include?(new_resource.command)
-    Chef::Log.debug('Matched docker container: ' + dps_line)
+    Chef::Log.debug('Matched docker container: ' + dps_line.squeeze(' '))
     ps = dps_line.split(/\s\s+/)
     name = ps[6] || ps[5]
     @current_resource.container_name(name)
