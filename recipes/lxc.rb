cb77d39cc40342196f6d60b1c49fa917eb3c0593
Tom Duffield <tom@opscode.com>
2014-11-06 09:29:07 -0600

    LXC fixes
    1) Condense LXC package installation instructions
    2) Implement workaround to upgrade libcgmanager on Ubuntu 14.04 due to lxc/lxc#247.

--- recipes/lxc.rb
+++ recipes/lxc.rb
@@ -1,10 +1,20 @@
 # TODO: Platforms handled here should be fixed in lxc cookbook
 # Currently: https://github.com/hw-cookbooks/lxc/
 case node['platform']
-when 'debian'
-  package 'lxc'
-when 'oracle'
+when 'debian', 'fedora', 'oracle'
   package 'lxc'
 when 'ubuntu'
+
+  #
+  # This is a workaround to address bflad/chef-docker#188. In the absence of an
+  # upstream solution we are manually upgrading libcgmanager to avoid lxc/lxc#247.
+  # In this future this issue should be addressed formally in the hw-cookbooks/lxc
+  # cookbook.
+  #
+  package 'libcgmanager0' do
+    action :upgrade
+    only_if { node['platform_version'] == '14.04' }
+  end
+
   include_recipe 'lxc'
 end
