826e40d783c894e79ee36583d092a35730d907c8
Christian Höltje <docwhat@gerf.org>
2015-04-20 14:00:07 -0400

    Fixed remaining `-notrunc` usages
    The `-notrunc` flag causes a very annoying warning message to stderr.
    
    This replaces the last usage (sysv init file), a comment, and the
    minitests.

--- test/cookbooks/docker_test/files/default/tests/minitest/support/helpers.rb
+++ test/cookbooks/docker_test/files/default/tests/minitest/support/helpers.rb
@@ -9,13 +9,13 @@ module Helpers
     include MiniTest::Chef::Resources
 
     def container_exists?(image, command = nil)
-      dps = shell_out("docker ps -a -notrunc")
+      dps = shell_out("docker ps -a --no-trunc")
       return dps.stdout.include?(command) if command
       dps.stdout.include?(image)
     end
 
     def container_running?(image, command = nil)
-      dps = shell_out("docker ps -a -notrunc")
+      dps = shell_out("docker ps -a --no-trunc")
       dps.stdout.each_line do |dps_line|
         if command
           return dps_line.include?("Up") if dps_line.include?(command)
