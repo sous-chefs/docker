ffdeac43155aa9ca06e7ff9737872031f9657757
Brandon Raabe <brandon.raabe@ge.com>
2015-08-14 22:43:16 -0700

    update port attribute to accept an array of ports

---
+++ libraries/_port_binding.rb
@@ -0,0 +1,48 @@
+class PortBinding
+  attr_reader :host_ip, :host_port, :container_port
+
+  def initialize(value)
+    @definition = value
+    @host_ip = nil
+    @host_port = nil
+    @container_port = nil
+    parse(value)
+  end
+
+  def binding
+    {
+      container_port.to_s => [
+        {
+          'HostIp' => host_ip,
+          'HostPort' => host_port
+        }
+      ]
+    }
+  end
+
+  def exposure
+    { container_port.to_s => {} }
+  end
+
+  def to_s
+    @definition
+  end
+
+  private
+
+  def parse(value)
+    parts = value.split(':')
+    case parts.length
+    when 3
+      @host_ip = parts[0]
+      @host_port = parts[1]
+      @container_port = parts[2]
+    when 2
+      @host_ip = '0.0.0.0'
+      @host_port = parts[0]
+      @container_port = parts[1]
+    when 1
+      @container_port = parts[0]
+    end
+  end
+end
