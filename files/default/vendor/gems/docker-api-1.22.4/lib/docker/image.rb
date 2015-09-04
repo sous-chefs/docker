234c31109e2adb82b61a03ecf2c4a3a2e96d8587
Sean OMeara <sean@chef.io>
2015-09-04 18:12:04 +0200

    patching docker-api headers

--- files/default/vendor/gems/docker-api-1.22.4/lib/docker/image.rb
+++ files/default/vendor/gems/docker-api-1.22.4/lib/docker/image.rb
@@ -104,7 +104,7 @@ class Docker::Image
     # Create a new Image.
     def create(opts = {}, creds = nil, conn = Docker.connection, &block)
       credentials = creds.nil? ? Docker.creds : creds.to_json
-      headers = credentials && Docker::Util.build_auth_header(credentials) || {}
+      headers = {}
       body = ''
       conn.post(
         '/images/create',
