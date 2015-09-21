cb6f637de91497840f6c6181de84635a9da26abd
Sean OMeara <sean@chef.io>
2015-09-21 15:28:11 +0200

    setting docker_image default read_timeout to 60

--- libraries/resource_docker_image.rb
+++ libraries/resource_docker_image.rb
@@ -15,7 +15,7 @@ class Chef
       attribute :force, kind_of: [TrueClass, FalseClass], default: false
       attribute :nocache, kind_of: [TrueClass, FalseClass], default: false
       attribute :noprune, kind_of: [TrueClass, FalseClass], default: false
-      attribute :read_timeout, kind_of: [Fixnum, NilClass], default: nil
+      attribute :read_timeout, kind_of: [Fixnum, NilClass], default: 60
       attribute :repo, kind_of: String, name_attribute: true
       attribute :rm, kind_of: [TrueClass, FalseClass], default: true
       attribute :source, kind_of: String
