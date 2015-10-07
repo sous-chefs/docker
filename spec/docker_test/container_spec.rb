544050211400a00d0ee320f7db785b23821473e9
Sean OMeara <sean@chef.io>
2015-10-07 17:35:21 -0400

    adding properties to spec for docker_container[hello-world]

--- spec/docker_test/container_spec.rb
+++ spec/docker_test/container_spec.rb
@@ -17,10 +17,69 @@ describe 'docker_test::container' do
   end
 
   context 'when compiling the recipe' do
-    
+
     it 'create docker_container[hello-world]' do
-      expect(chef_run).to create_docker_container('hello-world')
+      expect(chef_run).to create_docker_container('hello-world').with(
+        container_name: 'hello-world',
+        repo: nil,
+        tag: 'latest',
+        command: '/hello',
+        api_retries: 0, # expecting 3
+        attach_stderr: true,
+        attach_stdin: false,
+        attach_stdout: true,
+        autoremove: false,
+        binds: nil,
+        cap_add: nil,
+        cap_drop: nil,
+        cgroup_parent: '',
+        cpu_shares: 0,
+        cpuset_cpus: '',
+        detach: true,
+        devices: nil,
+        dns: nil,
+        dns_search: nil,
+        domain_name: '',
+        entrypoint: nil,
+        env: nil,
+        extra_hosts: nil,
+        exposed_ports: nil,
+        force: false,
+        host: nil,
+        host_name: nil,
+        labels: nil,
+        links: nil,
+        log_config: nil,
+        log_driver: nil,
+        log_opts: [],
+        mac_address: '',
+        memory: 0,
+        memory_swap: -1,
+        network_disabled: false,
+        network_mode: nil,
+        open_stdin: false,
+        outfile: nil,
+        port: nil,
+        port_bindings: nil,
+        privileged: false,
+        publish_all_ports: false,
+        read_timeout: 60,
+        remove_volumes: false,
+        restart_maximum_retry_count: 0,
+        restart_policy: 'no',
+        security_opts: [''],
+        signal: 'SIGKILL',
+        stdin_once: false,
+        timeout: nil,
+        tty: false,
+        ulimits: nil,
+        user: '',
+        volumes: nil,
+        volumes_from: nil,
+        working_dir: nil,
+        write_timeout: nil
+        )
     end
-    
+
   end
 end
