915e4be905cf2a6bf69f73f8cc1d06b00fd8783a
Sean OMeara <sean@chef.io>
2015-10-07 17:02:10 -0400

    - Fixing docker_image[hello-world] spec. I wrote it aginst the docker_container resource. - Derp.

--- spec/docker_test/image_spec.rb
+++ spec/docker_test/image_spec.rb
@@ -15,67 +15,20 @@ describe 'docker_test::image' do
     stub_command("[ ! -z `docker ps -qaf 'name=registry_proxy$'` ]").and_return(true)
   end
 
-  # I found some weird behavior with Chef 12.4.3.
   context 'when compiling the recipe' do
     it 'pulls docker_image[hello-world]' do
       expect(chef_run).to pull_docker_image('hello-world').with(
-        container_name: nil, # expected 'hello-world'
-        repo: 'hello-world',
-        tag: 'latest',
-        command: nil, # expected ''
-        api_retries: 3, 
-        attach_stderr: nil, # expected true
-        attach_stdin: nil, # expected false
-        attach_stdout: nil, # expected true
-        autoremove: nil, # expected false
-        binds: nil, 
-        cap_add: nil,
-        cap_drop: nil,
-        cgroup_parent: nil, # expecting ''
-        cpu_shares: nil, # expecting 0
-        cpuset_cpus: nil, # expecting ''
-        detach: nil, # expecting true
-        devices: nil,
-        dns: nil,
-        dns_search: nil,
-        domain_name: nil, # expecting ''
-        entrypoint: nil,
-        env: nil,
-        extra_hosts: nil,
-        exposed_ports: nil,
+        api_retries:  3,
+        destination: nil,
         force: false,
         host: nil,
-        host_name: nil,
-        labels: nil,
-        links: nil,
-        log_config: nil,
-        log_driver: nil,
-        log_opts: nil, # expecting []
-        mac_address: nil, # expecting ''
-        memory: nil, # expecting 0
-        memory_swap: nil, # expecting -1
-        network_disabled: nil, # expecting false
-        network_mode: nil,
-        open_stdin: nil, # expecting false
-        outfile: nil,
-        port: nil,
-        port_bindings: nil,
-        privileged: nil, # expecting false
-        publish_all_ports: nil, # expecting false
-        read_timeout: 120, # expecting 60
-        remove_volumes: nil, # expecting false
-        restart_maximum_retry_count: nil, # expecting 0
-        restart_policy: nil, # expecting 'no'
-        security_opts: nil, # expecting ['']
-        signal: nil, # expecting 'SIGKILL'
-        stdin_once: nil, # expecting false
-        # timeout: nil, # causes a chefspec error
-        tty: nil, # expecting false
-        ulimits: nil,
-        user: nil, # expecting ''
-        volumes: nil,
-        volumes_from: nil,
-        working_dir: nil,
+        nocache: false,
+        noprune:  false,
+        read_timeout: 120,
+        repo: 'hello-world',
+        rm: true,
+        source: nil,
+        tag: 'latest',
         write_timeout: nil
         )
     end
