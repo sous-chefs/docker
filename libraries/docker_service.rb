e34d428d9430d941d5c77487c04dd5851c7cbf6a
John Keiser <john@johnkeiser.com>
2015-10-06 17:45:42 -0700

    Use defaults and coercion instead of helper methods in service

--- libraries/docker_service.rb
+++ libraries/docker_service.rb
@@ -10,10 +10,40 @@ class Chef
       provides :docker_service
 
       # service installation
-      property :source, String, default: nil
-      property :version, String, default: nil
-      property :checksum, String, default: nil
-
+      property :source, String, default: lazy {
+        "https://get.docker.com/builds/#{docker_kernel}/#{docker_arch}/docker-#{version}"
+      }
+      property :version, String, default: lazy {
+        if node['platform'] == 'amazon' ||
+           node['platform'] == 'ubuntu' && node['platform_version'].to_f < 15.04 ||
+           node['platform_family'] == 'rhel' && node['platform_version'].to_i < 7 ||
+           node['platform_family'] == 'debian' && node['platform_version'].to_i <= 7
+          '1.6.2'
+        else
+          '1.8.2'
+        end
+      }
+      property :checksum, String, default: lazy {
+        case docker_kernel
+        when 'Darwin'
+          case version
+          when '1.6.0' then '9e960e925561b4ec2b81f52b6151cd129739c1f4fba91ce94bdc0333d7d98c38'
+          when '1.6.2' then 'f29b8b2185c291bd276f7cdac45a674f904e964426d5b969fda7b8ef6b8ab557'
+          when '1.7.0' then '1c8ee59249fdde401afebc9a079cb75d7674f03d2491789fb45c88020a8c5783'
+          when '1.7.1' then 'b8209b4382d0b4292c756dd055c12e5efacec2055d5900ac91efc8e81d317cf9'
+          when '1.8.1' then '0f5db35127cf14b57614ad7513296be600ddaa79182d8d118d095cb90c721e3a'
+          when '1.8.2' then 'cef593612752e5a50bd075931956075a534b293b7002892072397c3093fe11a6'
+          end
+        when 'Linux'
+          case version
+          when '1.6.0' then '526fbd15dc6bcf2f24f99959d998d080136e290bbb017624a5a3821b63916ae8'
+          when '1.6.2' then 'e131b2d78d9f9e51b0e5ca8df632ac0a1d48bcba92036d0c839e371d6cf960ec'
+          when '1.7.1' then '4d535a62882f2123fb9545a5d140a6a2ccc7bfc7a3c0ec5361d33e498e4876d5'
+          when '1.8.1' then '843f90f5001e87d639df82441342e6d4c53886c65f72a5cc4765a7ba3ad4fc57'
+          when '1.8.2' then '97a3f5924b0b831a310efa8bf0a4c91956cd6387c4a8667d27e2b2dd3da67e4d'
+          end
+        end
+      }
       # daemon runtime arguments
       property :instance, String, name_property: true, required: true
       property :api_cors_header, String, default: nil
@@ -21,14 +51,25 @@ class Chef
       property :bip, [IPV4_ADDR, IPV4_CIDR, IPV6_ADDR, IPV6_CIDR], default: nil
       property :debug, [true, false], default: nil
       property :daemon, [true, false], default: true
-      property :dns, [String, Array], default: []
+      property :dns, Array, coerce: proc { |v| Array(v) }
       property :dns_search, Array, default: nil
       property :exec_driver, %w(native lxc), default: nil
       property :fixed_cidr, String, default: nil
       property :fixed_cidr_v6, String, default: nil
       property :group, String, default: nil
       property :graph, String, default: nil
-      property :host, [String, Array], default: nil
+      property :host, [String, Array], coerce: proc { |v|
+        v = v.split if v.is_a?(String)
+        r = []
+        Array(v).each do |s|
+          if s.match(/^unix:/) || s.match(/^tcp:/) || s.match(/^fd:/)
+            r << s
+          else
+            Chef::Log.info("WARNING: docker_service host property #{s} not valid")
+          end
+        end
+        r
+      }
       property :icc, [true, false], default: nil
       property :insecure_registry, String, default: nil
       property :ip, [IPV4_ADDR, IPV6_ADDR], default: nil
@@ -41,13 +82,13 @@ class Chef
       property :log_level, [:debug, :info, :warn, :error, :fatal], default: nil
       property :label, String, default: nil
       property :log_driver, %w( json-file syslog journald gelf fluentd none ), default: nil
-      property :log_opts, [String, Array], default: []
+      property :log_opts, Array, coerce: proc { |v| Array(v) }
       property :mtu, String, default: nil
-      property :pidfile, String, default: nil
+      property :pidfile, String, default: lazy { "/var/run/#{docker_name}.pid" }
       property :registry_mirror, String, default: nil
-      property :storage_driver, [String, Array], default: nil
+      property :storage_driver, Array, coerce: proc { |v| Array(v) }
       property :selinux_enabled, [true, false], default: nil
-      property :storage_opts, [String, Array], default: []
+      property :storage_opts, Array, coerce: proc { |v| Array(v) }
       property :tls, [true, false], default: nil
       property :tls_verify, [true, false], default: nil
       property :tls_ca_cert, String, default: nil
@@ -55,7 +96,7 @@ class Chef
       property :tls_server_key, String, default: nil
       property :tls_client_cert, String, default: nil
       property :tls_client_key, String, default: nil
-      property :default_ulimit, [String, Array], default: nil
+      property :default_ulimit, Array, coerce: proc { |v| Array(v) }
       property :userland_proxy, [true, false], default: nil
 
       # environment variables to set before running daemon
@@ -72,6 +113,20 @@ class Chef
       alias_method :tlskey, :tls_server_key
       alias_method :tlsverify, :tls_verify
 
+      # Helpers
+      protected
+
+      def docker_name
+        'docker'
+      end
+      def docker_kernel
+        node['kernel']['name']
+      end
+      def docker_arch
+        node['kernel']['machine']
+      end
+
+
       default_action :create
 
       declare_action_class.class_eval do
@@ -83,8 +138,8 @@ class Chef
       action :create do
         # Pull a precompiled binary off the network
         remote_file docker_bin do
-          source parsed_source
-          checksum parsed_checksum
+          source new_resource.source
+          checksum new_resource.checksum
           owner 'root'
           group 'root'
           mode '0755'
@@ -113,9 +168,9 @@ class Chef
         def load_current_resource
           @current_resource = Chef::Resource::DockerService.new(name)
 
-          Docker.url = parsed_connect_host if parsed_connect_host
+          Docker.url = connect_host if connect_host
 
-          if parsed_connect_host =~ /^tcp:/ && tls_ca_cert
+          if connect_host =~ /^tcp:/ && tls_ca_cert
             Docker.options = {
               ssl_ca_file: tls_ca_cert,
               client_cert: tls_client_cert,
--- libraries/helpers_service.rb
+++ libraries/helpers_service.rb
@@ -27,70 +27,14 @@ IPV4_CIDR ||= %r{(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[
 module DockerHelpers
   module Service
     # Path to docker executable
-    def docker_arch
-      node['kernel']['machine']
-    end
-
     def docker_bin
       '/usr/bin/docker'
     end
 
-    def docker_kernel
-      node['kernel']['name']
-    end
-
-    def docker_name
-      'docker'
-    end
-
-    def parsed_checksum
-      return checksum if checksum
-      case docker_kernel
-      when 'Darwin'
-        case parsed_version
-        when '1.6.0' then '9e960e925561b4ec2b81f52b6151cd129739c1f4fba91ce94bdc0333d7d98c38'
-        when '1.6.2' then 'f29b8b2185c291bd276f7cdac45a674f904e964426d5b969fda7b8ef6b8ab557'
-        when '1.7.0' then '1c8ee59249fdde401afebc9a079cb75d7674f03d2491789fb45c88020a8c5783'
-        when '1.7.1' then 'b8209b4382d0b4292c756dd055c12e5efacec2055d5900ac91efc8e81d317cf9'
-        when '1.8.1' then '0f5db35127cf14b57614ad7513296be600ddaa79182d8d118d095cb90c721e3a'
-        when '1.8.2' then 'cef593612752e5a50bd075931956075a534b293b7002892072397c3093fe11a6'
-        end
-      when 'Linux'
-        case parsed_version
-        when '1.6.0' then '526fbd15dc6bcf2f24f99959d998d080136e290bbb017624a5a3821b63916ae8'
-        when '1.6.2' then 'e131b2d78d9f9e51b0e5ca8df632ac0a1d48bcba92036d0c839e371d6cf960ec'
-        when '1.7.1' then '4d535a62882f2123fb9545a5d140a6a2ccc7bfc7a3c0ec5361d33e498e4876d5'
-        when '1.8.1' then '843f90f5001e87d639df82441342e6d4c53886c65f72a5cc4765a7ba3ad4fc57'
-        when '1.8.2' then '97a3f5924b0b831a310efa8bf0a4c91956cd6387c4a8667d27e2b2dd3da67e4d'
-        end
-      end
-    end
-
-    def parsed_pidfile
-      return pidfile if pidfile
-      "/var/run/#{docker_name}.pid"
-    end
-
-    def parsed_version
-      return version if version
-      return '1.6.2' if node['platform'] == 'amazon'
-      return '1.6.2' if node['platform'] == 'ubuntu' && node['platform_version'].to_f < 15.04
-      return '1.6.2' if node['platform_family'] == 'rhel' && node['platform_version'].to_i < 7
-      return '1.6.2' if node['platform_family'] == 'debian' && node['platform_version'].to_i <= 7
-      '1.8.2'
-    end
-
     def docker_major_version
-      ray = parsed_version.split('.')
+      ray = version.split('.')
       ray.pop
       ray.push.join('.')
-    end
-
-    # https://get.docker.com/builds/Linux/x86_64/docker-1.8.2
-    # https://get.docker.com/builds/Darwin/x86_64/docker-1.8.2
-    def parsed_source
-      return source if source
-      "https://get.docker.com/builds/#{docker_kernel}/#{docker_arch}/docker-#{parsed_version}"
     end
 
     def docker_daemon_arg
@@ -129,49 +73,14 @@ module DockerHelpers
       end
     end
 
-    def parsed_dns
-      Array(dns)
-    end
-
-    # strip out invalid host arguments
-    def parsed_host
-      sockets = host.split if host.is_a?(String)
-      sockets = host if host.is_a?(Array)
-      r = []
-      sockets.each do |s|
-        if s.match(/^unix:/) || s.match(/^tcp:/) || s.match(/^fd:/)
-          r << s
-        else
-          Chef::Log.info("WARNING: docker_service host property #{s} not valid")
-        end
-      end
-      r
-    end
-
-    def parsed_log_opts
-      Array(log_opts)
-    end
-
-    def parsed_storage_driver
-      Array(storage_driver)
-    end
-
-    def parsed_storage_opts
-      Array(storage_opts)
-    end
-
-    def parsed_default_ulimit
-      Array(default_ulimit)
-    end
-
     def parsed_connect_host
-      parsed_host.first if host
+      host.first if host
     end
 
     def docker_opts
       opts = []
-      opts << "--host=#{parsed_connect_host}" if parsed_connect_host
-      if parsed_connect_host =~ /^tcp:/
+      opts << "--host=#{connect_host}" if connect_host
+      if connect_host =~ /^tcp:/
         opts << "--tls=#{tls}" unless tls.nil?
         opts << "--tlsverify=#{tls_verify}" unless tls_verify.nil?
         opts << "--tlscacert=#{tls_ca_cert}" if tls_ca_cert
@@ -187,15 +96,15 @@ module DockerHelpers
       opts << "--bridge=#{bridge}" if bridge
       opts << "--bip=#{bip}" if bip
       opts << '--debug' if debug
-      parsed_default_ulimit.each { |u| opts << "--default-ulimit=#{u}" } if default_ulimit
-      parsed_dns.each { |dns| opts << "--dns=#{dns}" }
+      default_ulimit.each { |u| opts << "--default-ulimit=#{u}" } if default_ulimit
+      dns.each { |dns| opts << "--dns=#{dns}" }
       dns_search.each { |dns| opts << "--dns-search=#{dns}" } if dns_search
       opts << "--exec-driver=#{exec_driver}" if exec_driver
       opts << "--fixed-cidr=#{fixed_cidr}" if fixed_cidr
       opts << "--fixed-cidr-v6=#{fixed_cidr_v6}" if fixed_cidr_v6
       opts << "--group=#{group}" if group
       opts << "--graph=#{graph}" if graph
-      parsed_host.each { |h| opts << "-H #{h}" } if host
+      host.each { |h| opts << "-H #{h}" } if host
       opts << "--icc=#{icc}" unless icc.nil?
       opts << "--insecure-registry=#{insecure_registry}" if insecure_registry
       opts << "--ip=#{ip}" if ip
@@ -206,13 +115,13 @@ module DockerHelpers
       opts << "--log-level=#{log_level}" if log_level
       opts << "--label=#{label}" if label
       opts << "--log-driver=#{log_driver}" if log_driver
-      parsed_log_opts.each { |log_opt| opts << "--log-opt=#{log_opt}" }
+      log_opts.each { |log_opt| opts << "--log-opt=#{log_opt}" }
       opts << "--mtu=#{mtu}" if mtu
       opts << "--pidfile=#{pidfile}" if pidfile
       opts << "--registry-mirror=#{registry_mirror}" if registry_mirror
-      parsed_storage_driver.each { |s| opts << "--storage-driver=#{s}" } if storage_driver
+      storage_driver.each { |s| opts << "--storage-driver=#{s}" } if storage_driver
       opts << '--selinux-enabled=true' if selinux_enabled
-      parsed_storage_opts.each { |storage_opt| opts << "--storage-opt=#{storage_opt}" }
+      storage_opts.each { |storage_opt| opts << "--storage-opt=#{storage_opt}" }
       opts << "--tls=#{tls}" unless tls.nil?
       opts << "--tlsverify=#{tls_verify}" unless tls_verify.nil?
       opts << "--tlscacert=#{tls_ca_cert}" if tls_ca_cert
