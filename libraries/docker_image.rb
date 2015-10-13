64ddfa56d9db37d36b4d38d68dc318960a032579
Sean OMeara <sean@chef.io>
2015-10-13 11:05:42 -0700

    Revert "Use with_retries and load_current_value in DockerImage"
    This reverts commit 09ffd95c4f33f89ddb715ee46a9d7f0776346a15.

--- libraries/docker_image.rb
+++ libraries/docker_image.rb
@@ -10,82 +10,77 @@ class Chef
       property :read_timeout, default: 120
 
       # https://docs.docker.com/reference/api/docker_remote_api_v1.20/
-      property :host, [String, nil]
-      property :repo, String, name_property: true, identity: true
-      property :tag, String, default: 'latest', identity: true
-      property :docker_image, Docker::Image, desired_state: false
-      property :force, Boolean, desired_state: false
-      # For remove
-      property :noprune, Boolean, desired_state: false
-      property :nocache, Boolean, desired_state: false
-      # For build
-      property :rm, Boolean, default: true, desired_state: false
-      property :source, String
-      # For save
       property :destination, [String, nil]
+      property :force, Boolean, default: false
+      property :host, [String, nil]
+      property :nocache, Boolean, default: false
+      property :noprune, Boolean, default: false
+      property :repo, String, name_property: true
+      property :rm, Boolean, default: true
+      property :source, String
+      property :tag, String, default: 'latest'
 
-      include DockerHelpers::Image
+      alias_method :image, :repo
+      alias_method :image_name, :repo
+      alias_method :no_cache, :nocache
+      alias_method :no_prune, :noprune
+
+      #########
+      # Actions
+      #########
 
       default_action :pull
 
+      declare_action_class.class_eval do
+        include DockerHelpers::Image
+      end
+
       action :build do
-        build_image
-        created_image = build_image
-        update_tag(created_image)
+        converge_by "Build image #{image_identifier}" do
+          build_image
+        end
       end
 
       action :build_if_missing do
-        return if current_resource
+        return if Docker::Image.exist?(image_identifier, {}, connection)
         action_build
       end
 
       action :import do
-        return if current_resource
-        converge_by "Import image #{image}" do
-          imported_image = with_retries { Docker::Image.import(source, {}, connection) }
+        return if Docker::Image.exist?(image_identifier, {}, connection)
+        converge_by "Import image #{image_identifier}" do
+          import_image
         end
-        update_tag(imported_image)
       end
 
       action :pull do
-        pull_image
+        # We already did the work, but we need to report what we did!
+        converge_by "Pull image #{image_identifier}" do
+        end if pull_image
       end
 
       action :pull_if_missing do
-        return if current_resource
+        return if Docker::Image.exist?(image_identifier, {}, connection)
         action_pull
       end
 
       action :push do
-        converge_by "Push image #{image}" do
-          with_retries { current_image.push }
+        converge_by "Push image #{image_identifier}" do
+          push_image
         end
       end
 
       action :remove do
-        if current_resource
-          converge_by "Remove image #{image}" do
-            with_retries { current_image.remove(force: force, noprune: noprune) }
-          end
+        return unless Docker::Image.exist?(image_identifier, {}, connection)
+        converge_by "Remove image #{image_identifier}" do
+          remove_image
         end
       end
 
       action :save do
-        converge_by "Save image #{image}" do
-          with_retries { Docker::Image.save(repo, destination, connection) }
+        converge_by "Save image #{image_identifier}" do
+          save_image
         end
-      end
-
-      load_current_value do
-        begin
-          with_retries { docker_image Docker::Image.get(image, connection) }
-        rescue Docker::Error::NotFoundError
-          current_value_does_not_exist!
-        end
-      end
-
-      action_class.class_eval do
-        include DockerHelpers::Image::Actions
       end
     end
   end
