50afb4d61719310c981081b13b96bbf886890585
Sean OMeara <sean@chef.io>
2015-10-17 15:15:49 -0700

    fixing rubocop violations

--- spec/docker_test/registry_spec.rb
+++ spec/docker_test/registry_spec.rb
@@ -14,7 +14,7 @@ describe 'docker_test::registry' do
     stub_command('/usr/bin/test -f /tmp/registry/tls/server.csr').and_return(false)
     stub_command("[ ! -z `docker ps -qaf 'name=registry_service$'` ]").and_return(false)
     stub_command("[ ! -z `docker ps -qaf 'name=registry_proxy$'` ]").and_return(false)
-    stub_command("nc -z -w5 localhost 5000 && nc -z -w5 localhost 5043").and_return(true)
+    stub_command('nc -z -w5 localhost 5000 && nc -z -w5 localhost 5043').and_return(true)
   end
 
   context 'when compiling the recipe' do
