15db7133e408ded9ec463c3ef6211b9bbb5585c1
Sean OMeara <sean@chef.io>
2015-11-11 00:34:23 -0500

    - Docker 1.9 no longer returns the image id in the create response. - Hacking around that fact by making an extra API call.

--- libraries/helpers_image.rb
+++ libraries/helpers_image.rb
@@ -68,7 +68,9 @@ module DockerCookbook
           creds = node.run_state['docker_auth'] && node.run_state['docker_auth'][registry_host] || (node.run_state['docker_auth'] ||= {})['index.docker.io']
 
           original_image = Docker::Image.get(image_identifier, {}, connection) if Docker::Image.exist?(image_identifier, {}, connection)
-          new_image = Docker::Image.create({ 'fromImage' => image_identifier }, creds, connection)
+          useless_data = Docker::Image.create({ 'fromImage' => image_identifier }, creds, connection)
+          new_image = Docker::Image.get(image_identifier, {}, connection) if Docker::Image.exist?(image_identifier, {}, connection)
+
           !(original_image && original_image.id.start_with?(new_image.id))
         end
       end
