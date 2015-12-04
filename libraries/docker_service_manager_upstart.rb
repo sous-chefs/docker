0909ad3c7d14018ce7c00997146ddf3a832e17bf
Sean OMeara <sean@chef.io>
2015-12-04 15:11:05 -0500

    multi service support for docker_service_manager_upstart

--- libraries/docker_service_manager_upstart.rb
+++ libraries/docker_service_manager_upstart.rb
@@ -5,28 +5,28 @@ module DockerCookbook
     provides :docker_service_manager, platform: 'ubuntu'
 
     action :start do
-      template '/etc/init/docker.conf' do
-        path '/etc/init/docker.conf'
+      template "/etc/init/#{docker_name}.conf" do
         source 'upstart/docker.conf.erb'
         owner 'root'
         group 'root'
         mode '0644'
         variables(
           config: new_resource,
+          docker_name: docker_name,
           docker_cmd: docker_cmd,
           docker_daemon_cmd: docker_daemon_cmd
         )
         cookbook 'docker'
-        notifies :restart, new_resource unless ::File.exist? '/etc/docker-firstconverge'
+        notifies :restart, new_resource unless ::File.exist? "/etc/#{docker_name}-firstconverge"
         notifies :restart, new_resource if auto_restart
         action :create
       end
 
-      file '/etc/docker-firstconverge' do
+      file "/etc/#{docker_name}-firstconverge" do
         action :create
       end
 
-      service 'docker' do
+      service docker_name do
         provider Chef::Provider::Service::Upstart
         supports status: true
         action :start
@@ -34,7 +34,7 @@ module DockerCookbook
     end
 
     action :stop do
-      service 'docker' do
+      service docker_name do
         provider Chef::Provider::Service::Upstart
         supports status: true
         action :stop
---
+++ test/cookbooks/docker_test/recipes/service_upstart_multi.rb
@@ -0,0 +1,63 @@
+# installation
+docker_installation_binary 'default' do
+  action :create
+end
+
+# service named 'default'
+docker_service_manager_upstart 'default' do
+  graph '/var/lib/docker'
+  host 'unix:///var/run/docker.sock'
+  action :start
+end
+
+docker_image 'busybox' do
+  host 'unix:///var/run/docker.sock'
+end
+
+docker_container 'service default echo server' do
+  container_name 'an_echo_server'
+  repo 'busybox'
+  command 'nc -ll -p 7 -e /bin/cat'
+  port '7:7'
+  action :run
+end
+
+# service A
+docker_service_manager_upstart 'one' do
+  graph '/var/lib/docker-one'
+  host 'unix:///var/run/docker-one.sock'
+  action :start
+end
+
+docker_image 'hello-world' do
+  host 'unix:///var/run/docker-one.sock'
+  tag 'latest'
+end
+
+docker_container 'hello-world' do
+  host 'unix:///var/run/docker-one.sock'
+  command '/hello'
+  action :create
+end
+
+# service B
+docker_service_manager_upstart 'two' do
+  graph '/var/lib/docker-two'
+  host 'unix:///var/run/docker-two.sock'
+  action :start
+end
+
+docker_image 'alpine' do
+  host 'unix:///var/run/docker-two.sock'
+  tag '3.1'
+end
+
+docker_container 'service two echo_server' do
+  container_name 'an_echo_server'
+  repo 'alpine'
+  tag '3.1'
+  command 'nc -ll -p 7 -e /bin/cat'
+  port '7:7'
+  host 'unix:///var/run/docker-two.sock'
+  action :run
+end
