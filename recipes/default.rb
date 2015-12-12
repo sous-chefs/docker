5678754ca33aa76ee9c9e0bfca08d56b1003ed9a
Chase Bolt <cbolt@denirostaff.com>
2015-12-11 18:10:45 -0800

    remove dupe gem version

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,11 +1,13 @@
+gem_version = '1.24.1'
+
 if Chef::Resource::ChefGem.instance_methods(false).include?(:compile_time)
   chef_gem 'docker-api' do
-    version '1.24.1'
+    version gem_version
     compile_time true
   end
 else
   chef_gem 'docker-api' do
-    version '1.24.1'
+    version gem_version
     action :nothing
   end.run_action(:install)
 end
