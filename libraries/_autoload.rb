f55f89cfa11c6ba2c8c6fdbec3d5c9d31dea7ac2
Sean OMeara <sean@chef.io>
2015-12-17 20:23:49 -0500

    Revert "Allow distributed gems to be activated normally via Ruby"
    This reverts commit af2a7a194110672678c11252666d3dd081fe81a8.

--- libraries/_autoload.rb
+++ libraries/_autoload.rb
@@ -1,6 +1,3 @@
-# Set up rubygems to activate any gems we find herein.
-ENV['GEM_PATH'] = ([File.expand_path('../../files/default/vendor', __FILE__)] + Gem.path).join(Gem.path_separator)
-Gem.paths = ENV
-gem 'docker-api', '~> 1.24'
-
+$LOAD_PATH.push *Dir[File.expand_path('../../files/default/vendor/gems/**/lib', __FILE__)]
 $LOAD_PATH.unshift *Dir[File.expand_path('..', __FILE__)]
+
