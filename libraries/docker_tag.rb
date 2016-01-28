ec6ad6a8243cb7f2bce08411097128a88b2a17c3
Sergey Bahchissaraitsev <sergey@bahchis.com>
2016-01-28 14:09:10 +0200

    docker tag force as property

--- libraries/docker_tag.rb
+++ libraries/docker_tag.rb
@@ -1,22 +1,22 @@
 module DockerCookbook
-  class DockerTag < ChefCompat::Resource
+  class DockerTag < DockerBase
     use_automatic_resource_name
 
     property :target_repo, String, name_property: true
     property :target_tag, String
     property :to_repo, String
     property :to_tag, String
+    property :force, Boolean, default: false
 
     #########
     # Actions
     #########
 
     action :tag do
-      return if Docker::Image.exist?("#{to_repo}:#{to_tag}")
       begin
         converge_by "update #{target_repo}:#{target_tag} to #{to_repo}:#{to_tag}" do
           i = Docker::Image.get("#{target_repo}:#{target_tag}")
-          i.tag('repo' => to_repo, 'tag' => to_tag, 'force' => true)
+          i.tag('repo' => to_repo, 'tag' => to_tag, 'force' => force)
         end
       rescue Docker::Error => e
         raise e.message
