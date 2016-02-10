d763917c87dcf755300d64b10d32acffb4d60046
Chase Bolt <cbolt@denirostaff.com>
2016-02-10 14:42:48 -0800

    cops

--- libraries/helpers_network.rb
+++ libraries/helpers_network.rb
@@ -12,7 +12,7 @@ module DockerCookbook
         gateways = [] if gateways.empty?
         auxaddrs = [] if auxaddrs.empty?
         if subnets.size < ranges.size || subnets.size < gateways.size
-          fail 'every ip-range or gateway myust have a corresponding subnet'
+          raise 'every ip-range or gateway myust have a corresponding subnet'
         end
 
         data = {}
@@ -21,7 +21,7 @@ module DockerCookbook
         subnets.each do |s|
           data.each do |k, _|
             if subnet_matches(s, k) || subnet_matches(k, s)
-              fail 'multiple overlapping subnet configuration is not supported'
+              raise 'multiple overlapping subnet configuration is not supported'
             end
           end
           data[s] = { 'Subnet' => s, 'AuxAddress' => {} }
@@ -33,13 +33,13 @@ module DockerCookbook
             ok = subnet_matches(s, r)
             next unless ok
             if data[s]['IPRange'] != ''
-              fail 'cannot configure multiple ranges on the same subnet'
+              raise 'cannot configure multiple ranges on the same subnet'
             end
             data[s]['IPRange']
             match = true
           end
 
-          fail "no matching subnet for range #{r}" unless match
+          raise "no matching subnet for range #{r}" unless match
         end
 
         gateways.each do |g|
@@ -48,12 +48,12 @@ module DockerCookbook
             ok = subnet_matches(s, g)
             next unless ok
             unless data[s].fetch('Gateway', '').empty?
-              fail "cannot configure multiple gateways (#{g}, #{data[s]['Gateway']}) for the same subnet (#{s})"
+              raise "cannot configure multiple gateways (#{g}, #{data[s]['Gateway']}) for the same subnet (#{s})"
             end
             data[s]['Gateway'] = g
             match = true
           end
-          fail "no matching subnet for gateway #{s}" unless match
+          raise "no matching subnet for gateway #{s}" unless match
         end
 
         auxaddrs.each do |aa|
@@ -65,7 +65,7 @@ module DockerCookbook
             data[s]['AuxAddress'][key] = a
             match = true
           end
-          fail "no matching subnet for aux-address #{a}" unless match
+          raise "no matching subnet for aux-address #{a}" unless match
         end
 
         data.values
