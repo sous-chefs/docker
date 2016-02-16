f9b559c0e92f0bef1e9db5ba409bfa559b203d66
Sean OMeara <sean@chef.io>
2016-02-16 09:13:59 -0500

    cops

--- test/integration/network/serverspec/assert_functioning_spec.rb
+++ test/integration/network/serverspec/assert_functioning_spec.rb
@@ -14,12 +14,12 @@ end
 
 describe command('docker network inspect -f "{{ .Containers }}" network_a') do
   its(:exit_status) { should eq 0 }
-  its(:stdout) { should match "echo-station-network_a" }
+  its(:stdout) { should match 'echo-station-network_a' }
 end
 
 describe command('docker network inspect -f "{{ .Containers }}" network_a') do
   its(:exit_status) { should eq 0 }
-  its(:stdout) { should match "echo-base-network_a" }
+  its(:stdout) { should match 'echo-base-network_a' }
 end
 
 ###########
@@ -38,18 +38,18 @@ end
 
 describe command('docker network inspect -f "{{ .IPAM.Config }}" network_b') do
   its(:exit_status) { should eq 0 }
-  its(:stdout) { should match "192.168.88.0/24" }
-  its(:stdout) { should match "192.168.88.1" }
+  its(:stdout) { should match '192.168.88.0/24' }
+  its(:stdout) { should match '192.168.88.1' }
 end
 
 describe command('docker network inspect -f "{{ .Containers }}" network_b') do
   its(:exit_status) { should eq 0 }
-  its(:stdout) { should match "echo-station-network_b" }
+  its(:stdout) { should match 'echo-station-network_b' }
 end
 
 describe command('docker network inspect -f "{{ .Containers }}" network_b') do
   its(:exit_status) { should eq 0 }
-  its(:stdout) { should match "echo-base-network_b" }
+  its(:stdout) { should match 'echo-base-network_b' }
 end
 
 ###########
@@ -68,8 +68,8 @@ end
 
 describe command('docker network inspect -f "{{ .IPAM.Config }}" network_c') do
   its(:exit_status) { should eq 0 }
-  its(:stdout) { should match "a:192.168.89.2" }
-  its(:stdout) { should match "b:192.168.89.3" }
+  its(:stdout) { should match 'a:192.168.89.2' }
+  its(:stdout) { should match 'b:192.168.89.3' }
 end
 
 ###########
