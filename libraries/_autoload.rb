7318f901b04475b77bd80d07adc4e4ddb85def46
Chase Bolt <cbolt@denirostaff.com>
2016-02-25 17:03:25 -0800

    prevent serverspec from failing. bundler will handle it being installed for ci tests.

--- libraries/_autoload.rb
+++ libraries/_autoload.rb
@@ -1,2 +1,11 @@
-$LOAD_PATH.push *Dir[File.expand_path('../../files/default/vendor/gems/**/lib', __FILE__)]
-$LOAD_PATH.unshift *Dir[File.expand_path('..', __FILE__)]
+begin
+  gem 'docker-api', '= 1.26.2'
+rescue LoadError
+  run_context = Chef::RunContext.new(Chef::Node.new, {}, Chef::EventDispatch::Dispatcher.new)
+
+  require 'chef/resource/chef_gem'
+
+  docker = Chef::Resource::ChefGem.new('docker-api', run_context)
+  docker.version '= 1.26.2'
+  docker.run_action(:install)
+end
