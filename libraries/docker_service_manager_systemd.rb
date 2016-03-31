ebc4133446213469184602b5cfe3bcddf515bdf1
Steve Collins <steve.collins@oracle.com>
2016-03-31 08:43:05 -0600

    include support for oracle platform for docker_service_manager_systemd and docker_service_manager_sysvinit

--- libraries/docker_service_manager_systemd.rb
+++ libraries/docker_service_manager_systemd.rb
@@ -4,7 +4,7 @@ module DockerCookbook
 
     provides :docker_service_manager, platform: 'fedora'
 
-    provides :docker_service_manager, platform: %w(redhat centos scientific) do |node| # ~FC005
+    provides :docker_service_manager, platform: %w(redhat centos scientific oracle) do |node| # ~FC005
       node['platform_version'].to_f >= 7.0
     end
 
