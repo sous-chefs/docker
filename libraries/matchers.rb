447d8bcc517248ed50e448bae25b5a7c3a14bc8e
Jacob McCann <jacob.mccann2@target.com>
2016-09-13 09:28:02 -0500

    Add matchers for docker_installation_tarball

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -36,6 +36,14 @@ if defined?(ChefSpec)
     ChefSpec::Matchers::ResourceMatcher.new(:docker_installation_package, :delete, resource_name)
   end
 
+  def create_docker_installation_tarball(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:docker_installation_tarball, :create, resource_name)
+  end
+
+  def delete_docker_installation_tarball(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:docker_installation_tarball, :delete, resource_name)
+  end
+
   ################
   # docker_service
   ################
