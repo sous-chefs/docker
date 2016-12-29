69e93b5be51bece27c40695168cd551bb604c1ac
Sean OMeara <sean@sean.io>
2016-12-29 01:06:32 +0100

    adding more resource to auto test recipe

--- test/cookbooks/docker_test/recipes/auto.rb
+++ test/cookbooks/docker_test/recipes/auto.rb
@@ -1,4 +1,3 @@
-
 ################
 # Docker service
 ################
@@ -8,4 +7,15 @@ docker_service 'default' do
   install_method 'auto'
   service_manager 'auto'
   action [:create, :start]
+end
+
+docker_image 'alpine' do
+  action :pull
+end
+
+docker_container 'an_echo_server' do
+  repo 'alpine'
+  command 'nc -ll -p 7 -e /bin/cat'
+  port '7:7'
+  action :run
 end
