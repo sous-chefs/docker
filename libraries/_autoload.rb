8451f9dfe5f2aee3c378c7ed390fae2b2fd456ad
Sean OMeara <sean@sean.io>
2017-02-06 13:38:45 -0500

    Revert "vendoring docker-api-1.33.2"
    This reverts commit f02fd08e27f0c1b6d684ee233db1137d2a626e8b.

--- libraries/_autoload.rb
+++ libraries/_autoload.rb
@@ -1,17 +1,13 @@
-# begin
-#   gem 'docker-api', '= 1.33.0'
-# rescue LoadError
-#   unless defined?(ChefSpec)
-#     run_context = Chef::RunContext.new(Chef::Node.new, {}, Chef::EventDispatch::Dispatcher.new)
+begin
+  gem 'docker-api', '= 1.33.0'
+rescue LoadError
+  unless defined?(ChefSpec)
+    run_context = Chef::RunContext.new(Chef::Node.new, {}, Chef::EventDispatch::Dispatcher.new)
 
-#     require 'chef/resource/chef_gem'
+    require 'chef/resource/chef_gem'
 
-#     docker = Chef::Resource::ChefGem.new('docker-api', run_context)
-#     docker.version '= 1.33.0'
-#     docker.run_action(:install)
-#   end
-# end
-
-# 682 - vendoring
-$LOAD_PATH.push *Dir[File.expand_path('../../files/default/vendor/gems/**/lib', __FILE__)]
-$LOAD_PATH.unshift *Dir[File.expand_path('..', __FILE__)]
+    docker = Chef::Resource::ChefGem.new('docker-api', run_context)
+    docker.version '= 1.33.0'
+    docker.run_action(:install)
+  end
+end
