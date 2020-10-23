7bbd814c7d0202b005e98aa1f6bd350a4e922cfe
Lance Albertson <lance@osuosl.org>
2020-10-23 13:47:06 -0700

    Update tarball checksums to latest versions
    Also remove 17.12.0 as it's no longer needed.
    
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -17,6 +17,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Update metadata.rb to Sous Chefs
 - Update tarball version to 19.03.13
 - Update tarball suite tests
+- Update tarball checksums to latest versions
 
 ### Fixed
 
--- libraries/docker_installation_tarball.rb
+++ libraries/docker_installation_tarball.rb
@@ -41,18 +41,16 @@ module DockerCookbook
       case docker_kernel
       when 'Darwin'
         case version
-        when '17.12.0' then 'dc673421e0368c2c970203350a9d0cb739bc498c897e832779369b0b2a9c6192'
-        when '18.03.0' then '2d44ed2ac1e24cb22b6e72cb16d74fc9e60245a8ac1d4f79475604b804f46d38'
         when '18.03.1' then 'bbfb9c599a4fdb45523496c2ead191056ff43d6be90cf0e348421dd56bc3dcf0'
-        when '18.06.0' then '5489360ae1894375a56255fb821fcf368b33027cd4f4bbaebf5176c05b79f420'
+        when '18.06.3' then 'f7347ef27db9a438b05b8f82cd4c017af5693fe26202d9b3babf750df3e05e0c'
+        when '18.09.9' then 'ed83a3d51fef2bbcdb19d091ff0690a233aed4bbb47d2f7860d377196e0143a0'
         when '19.03.13' then 'd035d468218c26973710b35101b55dcf82c25d43a0a88aaa9f667b1782ec6ea4'
         end
       when 'Linux'
         case version
-        when '17.12.0' then '692e1c72937f6214b1038def84463018d8e320c8eaf8530546c84c2f8f9c767d'
-        when '18.03.0' then 'e5dff6245172081dbf14285dafe4dede761f8bc1750310156b89928dbf56a9ee'
         when '18.03.1' then '0e245c42de8a21799ab11179a4fce43b494ce173a8a2d6567ea6825d6c5265aa'
-        when '18.06.0' then '1c2fa625496465c68b856db0ba850eaad7a16221ca153661ca718de4a2217705'
+        when '18.06.3' then '346f9394393ee8db5f8bd1e229ee9d90e5b36931bdd754308b2ae68884dd6822'
+        when '18.09.9' then '82a362af7689038c51573e0fd0554da8703f0d06f4dfe95dd5bda5acf0ae45fb'
         when '19.03.13' then 'ddb13aff1fcdcceb710bf71a210169b9c1abfd7420eeaf42cf7975f8fae2fcc8'
         end
       end
