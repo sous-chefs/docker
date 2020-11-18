780740cc4d23834441a94a64670b8946c181ce54
m-zare <mostafazare@gmail.com>
2020-11-18 21:29:26 +0330

    fix test

--- test/cookbooks/docker_test/recipes/image.rb
+++ test/cookbooks/docker_test/recipes/image.rb
@@ -160,14 +160,18 @@ docker_image 'image_3' do
 end
 
 # build from Dockerfile with buildargs
-cookbook_file '/usr/local/src/container2/Dockerfile' do
+directory '/usr/local/src/container5' do
+  action :create
+end
+
+cookbook_file '/usr/local/src/container5/Dockerfile' do
   source 'Dockerfile_5'
   action :create
 end
 
 docker_image 'image_5' do
   tag 'v0.1.0'
-  source '/usr/local/src/container2/Dockerfile'
+  source '/usr/local/src/container5/Dockerfile'
   buildargs '{"IMAGE_NAME":"busybox","IMAGE_TAG":"latest"}'
   action :build_if_missing
 end
