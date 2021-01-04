e3a46ee6501d03601db259df9b90b15e76a0485e
Lance Albertson <lance@osuosl.org>
2021-01-04 14:07:26 -0800

    Debian 9 doesn't include 20.10 builds
    This works around it for testing until it gets EOL by Debian.
    
    Also remove unused reference to Amazon Linux with the InSpec test and
    node['docker']['version'] attribute in the `installation_package` suite.
    
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- test/cookbooks/docker_test/recipes/installation_package.rb
+++ test/cookbooks/docker_test/recipes/installation_package.rb
@@ -1,5 +1,15 @@
-docker_installation_package 'default' do
+docker_ver =
   # Include epoch on RHEL to fix idempotency issues
-  version platform_family?('rhel', 'fedora') ? '3:20.10.1' : '20.10.1'
+  if platform_family?('rhel', 'fedora')
+    '3:20.10.1'
+  # Debian 9 does not include 20.10
+  elsif platform?('debian') && node['platform_version'].to_i == 9
+    '19.03.14'
+  else
+    '20.10.1'
+  end
+
+docker_installation_package 'default' do
+  version docker_ver
   action :create
 end
