f88aba01f8ca18c53328016ef89e8d84e85d56e8
Lance Albertson <lance@osuosl.org>
2021-01-04 13:10:32 -0800

    Update to use 20.10 by default
    Docker 20.10.x was released in December with the latest version being 20.10.1.
    This updates everything to start using and testing this release.
    
    In addition, update tarball for 19.03 to 19.03.14.
    
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- test/cookbooks/docker_test/recipes/installation_package.rb
+++ test/cookbooks/docker_test/recipes/installation_package.rb
@@ -1,5 +1,5 @@
 docker_installation_package 'default' do
   # Include epoch on RHEL to fix idempotency issues
-  version platform_family?('rhel', 'fedora') ? '3:19.03.13' : '19.03.13'
+  version platform_family?('rhel', 'fedora') ? '3:20.10.1' : '20.10.1'
   action :create
 end
