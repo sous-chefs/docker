a73eef1a80c6603ec293d1b7996b337699a33a8f
Dan Webb <dan.webb@damacus.io>
2021-10-20 10:58:09 +0100

    Fix tests
    Signed-off-by: Dan Webb <dan.webb@damacus.io>

--- spec/libraries/image_prune_spec.rb
+++ spec/libraries/image_prune_spec.rb
@@ -1,27 +1,23 @@
 # TODO: Refactor test
-# require 'spec_helper'
-# require_relative '../../libraries/helpers_json'
+require 'spec_helper'
+require_relative '../../libraries/helpers_json'
 
-# RSpec.describe DockerCookbook::DockerHelpers::Json do
-#   class DummyClass < Chef::Node
-#     include DockerCookbook::DockerHelpers::Json
-#   end
+RSpec.describe DockerCookbook::DockerHelpers::Json do
+  class DummyClass < Chef::Node
+    include DockerCookbook::DockerHelpers::Json
+  end
 
-#   subject { DummyClass.new }
+  subject { DummyClass.new }
 
-#   describe '#generate_json' do
-#     it 'generates filter json' do
-#       # include DockerCookbook::DockerHelpers::Json
-#       expected = 'filters=%7B%22dangling%22%3A%7B%22true%22%3Atrue%7D%2C%22until%22%3A%7B%221h30m%22%3Atrue%7D%2C%22label%22%3A%7B%22com.example.vendor%3DACME%22%3Atrue%7D%2C%22label%21%22%3A%7B%22no_prune%22%3Atrue%7D%7D'
+  describe '#generate_json' do
+    it 'generates filter json' do
+      dangling = true
+      prune_until = '1h30m'
+      with_label = 'com.example.vendor=ACME'
+      without_label = 'no_prune'
+      expected = 'filters=%7B%22dangling%22%3A%7B%22true%22%3Atrue%7D%2C%22until%22%3A%7B%221h30m%22%3Atrue%7D%2C%22label%22%3A%7B%22com.example.vendor%3DACME%22%3Atrue%7D%2C%22label%21%22%3A%7B%22no_prune%22%3Atrue%7D%7D'
 
-#       dangling = true
-#       prune_until = '1h30m'
-#       with_label = 'com.example.vendor=ACME'
-#       without_label = 'no_prune'
-
-#       actual = generate_json(dangling, prune_until, with_label, without_label)
-
-#       expect(actual).to eq(expected)
-#     end
-#   end
-# end
+      expect(subject.generate_json(dangling, prune_until, with_label, without_label)).to eq(expected)
+    end
+  end
+end
