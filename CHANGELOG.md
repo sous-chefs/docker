119f3113a1a337b547df6504a15f30356657053b
Benjamin M. Hughes <bmhughes@bmhughes.co.uk>
2023-04-20 13:27:43 +0100

    Add volume_prune resource
    Issue #1240

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## Unreleased
 
+- Add volume_prune resource
+
 ## 11.6.1 - *2024-12-10*
 
 ## 11.6.0 - *2024-12-03*
--- libraries/helpers_json.rb
+++ libraries/helpers_json.rb
@@ -1,12 +1,14 @@
 module DockerCookbook
   module DockerHelpers
     module Json
-      def generate_json(dangling, prune_until = nil, with_label = nil, without_label = nil)
+      def prune_generate_json(dangling:, prune_until: nil, with_label: nil, without_label: nil, all: false)
         opts = { dangling: { "#{dangling}": true } }
         opts['until'] = { "#{prune_until}": true } if prune_until
         opts['label'] = { "#{with_label}": true } if with_label
         opts['label!'] = { "#{without_label}": true } if without_label
-        'filters=' + URI.encode_www_form_component(opts.to_json)
+        opts['all'] = true if all
+
+        'filters=' + URI.encode_www_form_component(opts.to_jso)
       end
     end
   end
