0769a8cea324b2e518795b23932dace92f8697b2
Dan Webb <dan.webb@damacus.io>
2025-01-21 22:39:36 +0000

    Rename
    Signed-off-by: Dan Webb <dan.webb@damacus.io>

--- test/cookbooks/docker_test/recipes/installation_package.rb
+++ test/cookbooks/docker_test/recipes/installation_package.rb
@@ -1,3 +1,3 @@
 docker_installation_package 'default' do
-  version '27.0.4' # node['docker']['version'] if node['docker']['version']
+  # version node['docker']['version'] if node['docker']['version']
 end
