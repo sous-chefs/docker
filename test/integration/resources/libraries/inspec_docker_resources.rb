e2e6b9bf34e9c38076824718c613d0b8fc4b7d58
Dan Webb <dan.webb@damacus.io>
2026-07-01 14:58:53 +0100

    fix: load docker inspec resources from pack

---
+++ test/integration/resources/libraries/inspec_docker_resources.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+require 'inspec-docker-resources/resources/docker'
+require 'inspec-docker-resources/resources/docker_container'
+require 'inspec-docker-resources/resources/docker_image'
